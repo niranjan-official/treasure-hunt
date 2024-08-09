f59580b9820bbc091a3d22f0260ce36167cb81b0
Niranjan Sabarinath <niranjansabarinath1521@gmail.com>
2024-08-09 08:20:22 +0530

    Update loading.jsx

--- components/loading.jsx
+++ components/loading.jsx
@@ -5,7 +5,7 @@ const Loading = () => {
   return (
     <div className="h-screen w-full flex justify-center items-center login">
       <div className="flex justify-center items-center p-10 rounded-xl">
-        <AiOutlineLoading size={40} className="animate-spin" />
+        <AiOutlineLoading size={40} className="animate-spin text-white/60" />
       </div>
     </div>
   );
